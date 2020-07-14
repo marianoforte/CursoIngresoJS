b4798a1d48a271da1ae0f10b1595911bbd5d6b8c
marianoforte@live.com.ar <marianoforte@live.com.ar>
2020-07-14 18:56:33 -0300

    Ejercicio 6 actualizado

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -5,6 +5,19 @@ mostrar el resulto por medio de "ALERT"*/
 
 function sumar()
 {
-	alert("ok");
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(num1);
+	num2 = parseInt(num2);
+
+	num1 = txtIdNum1.value
+	num2 = txtIdNum2.value
+
+	resultado = num1 + num2;
+	
+	
+	alert(resultado);
 }
 
