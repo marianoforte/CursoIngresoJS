012546ef8a493b25b1b214e3bc65bae9b04f2d46
Mariano Forte <67985541+marianoforte@users.noreply.github.com>
2020-07-15 13:49:35 -0300

    update jsEntradaSalida-07.js

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,12 +5,30 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+  let num1;
+	let num2;
+	let resultado;
+	
+  num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	
+  resultado = num1+num2;
+	
+	alert("La suma es " + resultado);	
 }
 
 function restar()
 {
+  let num1;
+	let num2;
+	let resultado;
 	
+  num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	
+  resultado = num1-num2;
+	
+	alert("La resta es " + resultado);	
 }
 
 function multiplicar()
